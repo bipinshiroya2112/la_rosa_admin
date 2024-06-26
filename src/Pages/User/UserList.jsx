ac650d42c919d05cbb63d710a59efc2d305c5e7f
bipinshiroya2112 <bipinshiroya2112@gmail.com>
2024-06-26 21:39:31 +0530

    user delete task complte

--- src/Pages/User/UserList.jsx
+++ src/Pages/User/UserList.jsx
@@ -55,23 +55,27 @@ const UserList = () => {
     }
   };
   const [DeleteConfirmation, setDeleteConfirmation] = useState(false);
+  const [isDeleteUser, setIsDeleteUser] = useState(false);
   const [SelectedUser, setSelectedUser] = useState({});
 
+  const ConfirmUserDelete = async (selectUser) => {
+    await axiosInstanceAuth
+      .post(`admin/User/delete/${SelectedUser?.id}`)
+      .then((res) => {
+        if (res?.data?.status) {
+          GetAllUserData();
+          toast.success(res?.data?.message);
+        } else {
+          toast.error(res?.data?.message);
+        }
+        setIsDeleteUser(false);
+      })
+      .catch((err) => {
+        console.log("err --->", err);
+      });
+  }
+
   const ConfirmDelete = async (SelectedUser) => {
-    // await axiosInstanceAuth
-    //   .post(`admin/User/delete/${SelectedUser?.id}`)
-    //   .then((res) => {
-    //     if (res?.data?.status) {
-    //       GetAllUserData();
-    //       toast.success(res?.data?.message);
-    //     } else {
-    //       toast.error(res?.data?.message);
-    //     }
-    //     setDeleteConfirmation(false);
-    //   })
-    //   .catch((err) => {
-    //     console.log("err --->", err);
-    //   });
 
     await axiosInstanceAuth
       .post(`userBlock/${SelectedUser?.id}`)
@@ -137,19 +141,9 @@ const UserList = () => {
                   </div>
 
                   <div className="w-[20%] flex flex-row justify-center md:justify-start items-center gap-2">
-                    {/* <img
-                      src={deleteRed}
-                      alt="icon"
-                      className="hidden md:block border border-[#FA979A] bg-[#FFEAEF] rounded-full p-2 cursor-pointer"
-                      onClick={() => {
-                        setDeleteConfirmation(true);
-                        setSelectedUser(d);
-                      }}
-                    /> */}
                     <button
-                      className={`flex justify-center items-center gap-2 text-white font-medium text-xs md:text-sm rounded-3xl shadow-md hover:shadow-lg py-3 px-5 cursor-pointer ${
-                        d.isActive ? "bg-green-500" : "bg-[#E5002A]"
-                      }`}
+                      className={`flex justify-center items-center gap-2 text-white font-medium text-xs md:text-sm rounded-3xl shadow-md hover:shadow-lg py-3 px-5 cursor-pointer ${d.isActive ? "bg-green-500" : "bg-[#E5002A]"
+                        }`}
                       onClick={() => {
                         setDeleteConfirmation(true);
                         setSelectedUser(d);
@@ -157,6 +151,15 @@ const UserList = () => {
                     >
                       {d.isActive ? "Unblock" : "Block"}
                     </button>
+                    <img
+                      src={deleteRed}
+                      alt="icon"
+                      className="hidden md:block border border-[#FA979A] bg-[#FFEAEF] rounded-full p-2 cursor-pointer"
+                      onClick={() => {
+                        setIsDeleteUser(true);
+                        setSelectedUser(d);
+                      }}
+                    />
                   </div>
                 </div>
               ))}
@@ -174,10 +177,9 @@ const UserList = () => {
               {numbers.map((item, index) => (
                 <button
                   disabled={item === "..."}
-                  className={`py-[3px] px-3 bg-red-200 rounded-full ${
-                    currentPage === item &&
+                  className={`py-[3px] px-3 bg-red-200 rounded-full ${currentPage === item &&
                     "bg-red-600 text-white font-bold font-mono"
-                  }`}
+                    }`}
                   onClick={() => goToPage(item)}
                   key={index}
                 >
@@ -245,6 +247,55 @@ const UserList = () => {
                     className="bg-[#E5002A] text-white font-semibold uppercase text-sm px-6 py-3 rounded-lg shadow hover:bg-[#D80022] hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150 mx-2"
                     type="button"
                     onClick={(e) => setDeleteConfirmation(false)}
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-20 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
+      {/* {-------- Delete conformation Pop up ------------} */}
+      {isDeleteUser === true && (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[999999]  outline-none focus:outline-none border ">
+            <div className="relative min-w-[285px] md:min-w-[350px] max-w-[90%] mx-auto  my-10 shadow-black shadow-2xl">
+              {/* ------ Content ------ */}
+              <div className="border-0 rounded-lg shadow-2xl relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/* ------ Header ------ */}
+                <div className="grid place-items-center place-content-end">
+                  <button
+                    className="bg-transparent border-0 text-black opacity-9 text-2xl font-normal outline-none focus:outline-none mx-3 my-2"
+                    onClick={(e) => setIsDeleteUser(false)}
+                  >
+                    ×
+                  </button>
+                </div>
+                {/* ------ Body ------ */}
+                <div className="relative grid place-items-center px-6 md:px-10 py-4 flex-auto">
+                  <h3 className="text-black font-semibold text-lg md:text-xl  leading-relaxed text-center mt-2">
+                    Are You Sure ?
+                  </h3>
+                  <p className="text-black font-medium text-sm md:text-base leading-normal text-center mt-3">
+                    You want to delete this user
+                  </p>
+                </div>
+
+                {/* ------ Fotter ------ */}
+                <div className="flex justify-center items-center m-5">
+                  <button
+                    className="bg-[#009600] text-white font-semibold uppercase text-sm px-6 py-3 rounded-lg shadow hover:bg-[#008500] hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150 mx-2"
+                    type="button"
+                    onClick={(e) => ConfirmUserDelete(SelectedUser)}
+                  >
+                    Yes
+                  </button>
+                  <button
+                    className="bg-[#E5002A] text-white font-semibold uppercase text-sm px-6 py-3 rounded-lg shadow hover:bg-[#D80022] hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150 mx-2"
+                    type="button"
+                    onClick={(e) => setIsDeleteUser(false)}
                   >
                     No
                   </button>

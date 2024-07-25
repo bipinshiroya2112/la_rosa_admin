8d365144cab8202ef21727a98a6d71e7ef17a09e
bipinshiroya2112 <bipinshiroya2112@gmail.com>
2024-07-25 16:13:01 +0530

    table response set

--- src/Layouts/Layout1.jsx
+++ src/Layouts/Layout1.jsx
@@ -11,7 +11,7 @@ const Layout1 = ({ children }) => {
           <SideBar />
         </div>
         <div className="col-span-10 xl:col-span-8 mx-0 lg:mx-10">
-          <div className="min-h-[640px]">{children}</div>
+          <div className="min-h-[640px] overflow-x-scroll">{children}</div>
           <div className="block md:hidden px-5">
             <Help />
           </div>
